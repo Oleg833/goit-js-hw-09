2e53a590d285b7d967a888013f2d11e9742ab3f9
Oleg833 <56390811+Oleg833@users.noreply.github.com>
2023-02-23 00:38:50 +0000

    Deploying to gh-pages from @ Oleg833/goit-js-hw-09@ad20649f3873feea14fcd4651845a8d5d634583a 🚀

---
+++ 01-color-switcher.25bb5124.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){a=setInterval(n,1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}));var a=null;function n(){var a="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=a,t.disabled=!0,e.disabled=!1}}();
+//# sourceMappingURL=01-color-switcher.25bb5124.js.map
